b722493d9ac38f310d399642599f0fb9bb8c1f0b
Damars <damarsepti627@gmail.com>
2024-11-18 19:54:15 +0700

    add select language feature

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # simple-tts
+
+![Screenshot](./screenshot.png)
+
+A simple text-to-speech application using the [Google Text-to-Speech API](https://github.com/pndurette/gTTS) and Streamlit.
+
+## Installation
+1. Clone the repository
+2. (Optional) Create a virtual environment and activate it
+3. Install the required packages using this command:
+```bash
+pip install -r requirements.txt
+```
+4. Run the application using this command:
+```bash
+streamlit run app.py
+```
--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 import streamlit as st
 from gtts import gTTS
+from langs import _langs
 
-st.title("Text-to-Speech Simple App")
+st.title("Simple Text-to-Speech App 🗣️")
 
-text = st.text_area(label="Text Input", value="", placeholder="Enter your text here")
+input_placeholder = st.empty()
+output_placeholder = st.empty()
+
+# initiate session state variable
+if "output_audio" not in st.session_state:
+    st.session_state['output_audio'] = None
+if "status_message" not in st.session_state:
+    st.session_state['status_message'] = None
 
 def convert():
-    tts = gTTS(text, lang="id")
-    tts.save("output.mp3")
-    print(btn)
+    try:
+        text = st.session_state['text_input']
+        lang = st.session_state['lang_input']
 
-btn = st.button("Convert", on_click=convert)
+        if not text.strip():
+            st.session_state['status message'] = "Please enter some text! 🙏"
 
-if btn:
-    st.audio("output.mp3")
+        with output_placeholder.container():
+            with st.spinner("Converting... 🔄"):
+                tts = gTTS(text, lang=lang)
+                audio_file = "output.mp3"
+                tts.save(audio_file)
+
+                st.session_state['output_audio'] = audio_file
+                st.session_state['status_message'] = "Completed! ✅"
+    except Exception as e:
+        st.session_state['status_message'] = f"An error occurred 💢: {e}"
+
+with input_placeholder.container():
+    lang = st.selectbox("Select Language", _langs.keys(), key="lang_input", index=25)
+    text = st.text_area(label="Text Input", key="text_input", value="", placeholder="Enter your text here... 📝", height=200)
+    btn = st.button("Convert", on_click=convert)
+
+with output_placeholder.container():
+    if st.session_state['status_message']:
+        st.write(st.session_state['status_message'])
+    if st.session_state['output_audio']:
+        st.audio(st.session_state['output_audio'], format="audio/mp3", autoplay=True)
